a89f79ab6c8de6783e857f6fe9a5d196f7e32601
ezoltech <ezramamo13@gmail.com>
2023-10-17 16:18:01 +0300

    adding navbar for checking

---
+++ src/components/Nav.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Navbar } from "flowbite-react";
+export const Nav = () => {
+  return (
+    <div className="top-0 sticky z-20">
+      <Navbar fluid rounded>
+        <Navbar.Brand href="https://flowbite-react.com">
+          <img
+            src="/favicon.svg"
+            className="mr-3 h-6 sm:h-9"
+            alt="Flowbite React Logo"
+          />
+          <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+            Flowbite React
+          </span>
+        </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Navbar.Link href="#" active>
+            Home
+          </Navbar.Link>
+          <Navbar.Link href="#">About</Navbar.Link>
+          <Navbar.Link href="#">Services</Navbar.Link>
+          <Navbar.Link href="#">Pricing</Navbar.Link>
+          <Navbar.Link href="#">Contact</Navbar.Link>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+};
