efd906b2090179ec3c06041bc03f53921c8a4cc5
ezoltech <ezramamo13@gmail.com>
2023-10-19 17:45:30 +0300

    builded

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import { Nav } from "./components/Nav";
-
+import "./index.css";
 function App() {
   return (
     <div className="App">
       <Nav />
+      <h1>Hello World</h1>
+      <p className="">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis
+        perspiciatis exercitationem optio odit? Cumque aliquam nobis molestias.
+        Quaerat minima aliquam nulla officiis quis iure ad delectus laboriosam
+        deleniti excepturi. Illum?
+      </p>
     </div>
   );
 }
