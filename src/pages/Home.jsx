3228d19f72c7026f9de02bfd03e9c51b7d27af34
ezoltech <ezramamo13@gmail.com>
2023-11-19 16:47:24 +0300

    home page for test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { Hero } from "../components/Hero";
 import { Progress } from "../components/Progress";
+import { Carousels } from "../components/Carousel";
 // import { Overview } from "../components/Overview";
 const Home = () => {
   return (
     <div className="flex flex-col gap-2">
       <Hero />
       <Progress />
-      {/* <Overview />
       <Overview />
       <Overview />
-      <Overview /> */}
+      <Overview />
+      <Overview />
+      <Carousels />
     </div>
   );
 };
