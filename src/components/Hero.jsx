c336c9b23db76fece382615530f9b9c27bffbc1b
ezoltech <ezramamo13@gmail.com>
2024-01-11 16:42:02 +0300

    tryin to fix 2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Fade } from "react-reveal";
 export const Hero = () => {
   return (
     <div className="">
@@ -8,69 +7,59 @@ export const Hero = () => {
           <div className="grid gap-10 lg:grid-cols-2">
             <div className="flex flex-col justify-center md:pr-8 xl:pr-0 lg:max-w-lg">
               <div className="max-w-xl mb-6 ">
-                <Fade bottom delay={1000}>
-                  <h2 className="max-w-lg mb-6 font-kaushan text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
-                    Let us handle
-                    <br className="hidden md:block" />
-                    your next{" "}
-                    <span className="inline-block text-deep-purple-accent-400">
-                      destination
-                    </span>
-                  </h2>
-                </Fade>
-                <Fade bottom delay={1100}>
-                  <p className="text-base text-gray-700 md:text-lg font-kanit">
-                    Sed ut perspiciatis unde omnis iste natus error sit
-                    voluptatem accusantium doloremque laudantium, totam rem
-                    aperiam, eaque ipsa quae. explicabo.
-                  </p>
-                </Fade>
+                <h2 className="max-w-lg mb-6 font-kaushan text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+                  Let us handle
+                  <br className="hidden md:block" />
+                  your next{" "}
+                  <span className="inline-block text-deep-purple-accent-400">
+                    destination
+                  </span>
+                </h2>
+
+                <p className="text-base text-gray-700 md:text-lg font-kanit">
+                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                  accusantium doloremque laudantium, totam rem aperiam, eaque
+                  ipsa quae. explicabo.
+                </p>
               </div>
               <div>
-                <Fade bottom delay={1100}>
-                  <a
-                    href="/"
-                    aria-label=""
-                    className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+                <a
+                  href="/"
+                  aria-label=""
+                  className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800"
+                >
+                  Learn more
+                  <svg
+                    className="inline-block w-3 ml-2"
+                    fill="currentColor"
+                    viewBox="0 0 12 12"
                   >
-                    Learn more
-                    <svg
-                      className="inline-block w-3 ml-2"
-                      fill="currentColor"
-                      viewBox="0 0 12 12"
-                    >
-                      <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z" />
-                    </svg>
-                  </a>
-                </Fade>
+                    <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z" />
+                  </svg>
+                </a>
               </div>
             </div>
 
             <div className="flex items-center justify-center -mx-4 lg:pl-8">
               <div className="flex flex-col items-end px-3 ">
-                <Fade left delay={1200}>
-                  <img
-                    className="object-cover mb-6 rounded-xl shadow-lg h-28 sm:h-48 xl:h-56 w-28 sm:w-48 xl:w-56 bg-gray-500  shadow-gray-500/50 "
-                    src="https://images.pexels.com/photos/3184287/pexels-photo-3184287.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-                    alt=""
-                  />
-                </Fade>
-                <Fade bottom delay={1300}>
-                  <img
-                    className="object-cover w-20 h-20 rounded-xl shadow-lg sm:h-32 xl:h-40 sm:w-32 xl:w-40 bg-gray-500  shadow-gray-500/50 "
-                    src="https://images.pexels.com/photos/3182812/pexels-photo-3182812.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-                    alt=""
-                  />
-                </Fade>
+                <img
+                  className="object-cover mb-6 rounded-xl shadow-lg h-28 sm:h-48 xl:h-56 w-28 sm:w-48 xl:w-56 bg-gray-500  shadow-gray-500/50 "
+                  src="https://images.pexels.com/photos/3184287/pexels-photo-3184287.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+                  alt=""
+                />
+
+                <img
+                  className="object-cover w-20 h-20 rounded-xl shadow-lg sm:h-32 xl:h-40 sm:w-32 xl:w-40 bg-gray-500  shadow-gray-500/50 "
+                  src="https://images.pexels.com/photos/3182812/pexels-photo-3182812.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+                  alt=""
+                />
               </div>
               <div className="px-3">
-                <Fade right delay={1250}>
-                  <img
-                    className="object-cover w-40 h-40 rounded-xl shadow-lg sm:h-64 xl:h-80 sm:w-64 xl:w-80 bg-gray-500  shadow-gray-500/50 "
-                    src="https://images.pexels.com/photos/3182739/pexels-photo-3182739.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500"
-                    alt=""
-                  />
-                </Fade>
+                <img
+                  className="object-cover w-40 h-40 rounded-xl shadow-lg sm:h-64 xl:h-80 sm:w-64 xl:w-80 bg-gray-500  shadow-gray-500/50 "
+                  src="https://images.pexels.com/photos/3182739/pexels-photo-3182739.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500"
+                  alt=""
+                />
               </div>
             </div>
           </div>
