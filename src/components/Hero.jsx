52f4c77e09a429964036c9d9a0f485db537d1b27
ezoltech <ezramamo13@gmail.com>
2024-03-29 19:18:51 +0300

    adding stunning landing images

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -45,20 +45,20 @@ export const Hero = () => {
               <div className="flex flex-col items-end px-3 ">
                 <img
                   className="object-cover mb-6 rounded-xl shadow-lg h-28 sm:h-48 xl:h-56 w-28 sm:w-48 xl:w-56 bg-gray-500  shadow-gray-500/50 "
-                  src="https://images.pexels.com/photos/3184287/pexels-photo-3184287.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+                  src="/img_1.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
                   alt=""
                 />
 
                 <img
                   className="object-cover w-20 h-20 rounded-xl shadow-lg sm:h-32 xl:h-40 sm:w-32 xl:w-40 bg-gray-500  shadow-gray-500/50 "
-                  src="https://images.pexels.com/photos/3182812/pexels-photo-3182812.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+                  src="/img_2.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
                   alt=""
                 />
               </div>
               <div className="px-3">
                 <img
                   className="object-cover w-40 h-40 rounded-xl shadow-lg sm:h-64 xl:h-80 sm:w-64 xl:w-80 bg-gray-500  shadow-gray-500/50 "
-                  src="https://images.pexels.com/photos/3182739/pexels-photo-3182739.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500"
+                  src="/img_3.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500"
                   alt=""
                 />
               </div>
