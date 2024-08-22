fe4ae059eb6f3190759b54344f3c576aa2283929
Nurhussen <nuryassinm132@gmail.com>
2024-08-22 07:36:23 -0500

    adding thing

--- src/components/Overview.jsx
+++ src/components/Overview.jsx
@@ -1,20 +1,98 @@
-import React from "react";
+import React,{useRef,useState} from "react";
+import { Carousel } from "flowbite-react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import autoprefixer from "autoprefixer";
+import { FaChevronCircleRight, FaChevronCircleLeft } from "react-icons/fa";
 
 export const Overview = () => {
+  "use client";
+const sliderRef = useRef(null);
+
+const next = () => {
+  sliderRef.current.slickNext();
+};
+
+const previous = () => {
+  sliderRef.current.slickPrev();
+};
+
+const sliderSettings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow:6,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 4,
+      },
+    },
+    {
+      breakpoint: 1366,
+      settings: {
+        slidesToShow: 4,
+      },
+    },
+    {
+      breakpoint: 1180,
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 955,
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 820,
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 710,
+      settings: {
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 640,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+    {
+      breakpoint: 370,
+      settings: {
+        slidesToShow: 1,
+      },
+    },
+  ],
+};
   return (
-    <div className="flex flex-col gap-3 ml-7 overflow-x-auto max-w-full font-kanit dark:bg-gray-950 dark:text-white transition-colors">
+    <div className="flex flex-col gap-3 ml-7   font-kanit dark:bg-gray-950 dark:text-white transition-colors w-full justify-center">
       <h5 style={{ textTransform: "uppercase" }} className="ml-3 dark:text-white">
         top rated
       </h5>
-
-      <div
-        className="grid overflow-x-auto xl:grid-cols-7 lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-5  justify-center"
-        style={{ width: "100%" }}
-      >
+      <div className="mt-4 flex">
+            <button className="font-bold py-2 px-4 rounded-l" onClick={previous}>
+              <FaChevronCircleLeft style={{ fontSize: "40px" }} />
+            </button>
+            <button className="font-bold py-2 px-4 rounded-r" onClick={next}>
+              <FaChevronCircleRight style={{ fontSize: "40px" }} />
+            </button>
+          </div>
+      <Slider {...sliderSettings} ref={sliderRef} className="flex">
         {/*  */}
-        <div className="flex flex-col dark:bg-gray-950 dark:text-white">
+        <div className="flex flex-col dark:bg-gray-950 dark:text-white ">
           <img
-            className="object-cover mb-6 rounded-xl shadow-lg h-28 sm:h-48 xl:h-56 w-28 sm:w-48 xl:w-56 shadow-[#777]"
+            className="object-cover mb-6 rounded-xl shadow-lg h-56 w-56 shadow-[#777]"
             src="https://images.pexels.com/photos/3184287/pexels-photo-3184287.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
             alt=""
           />
@@ -24,7 +102,7 @@ export const Overview = () => {
         {/* Example additional card element */}
         <div className="flex flex-col">
           <img
-            className="object-cover mb-6 rounded-xl shadow-lg h-28 sm:h-48 xl:h-56 w-28 sm:w-48 xl:w-56 shadow-[#777] dark:shadow-cyan-500/50"
+            className="object-cover mb-6 rounded-xl shadow-lg h-56 w-56 shadow-[#777] dark:shadow-cyan-500/50"
             src="https://images.pexels.com/photos/3184287/pexels-photo-3184287.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
             alt=""
           />
@@ -32,7 +110,7 @@ export const Overview = () => {
         </div>
         <div className="flex flex-col">
           <img
-            className="object-cover mb-6 rounded-xl shadow-lg h-28 sm:h-48 xl:h-56 w-28 sm:w-48 xl:w-56 shadow-[#777] dark:shadow-cyan-500/50"
+            className="object-cover mb-6 rounded-xl shadow-lg h-56 w-56 shadow-[#777] dark:shadow-cyan-500/50"
             src="https://images.pexels.com/photos/3184287/pexels-photo-3184287.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
             alt=""
           />
@@ -40,7 +118,7 @@ export const Overview = () => {
         </div>
         <div className="flex flex-col dark:bg-gray-950 dark:text-white">
           <img
-            className="object-cover mb-6 rounded-xl shadow-lg h-28 sm:h-48 xl:h-56 w-28 sm:w-48 xl:w-56 shadow-[#777] dark:shadow-cyan-500/50"
+            className="object-cover mb-6 rounded-xl shadow-lg h-56 w-56 shadow-[#777] dark:shadow-cyan-500/50"
             src="https://images.pexels.com/photos/3184287/pexels-photo-3184287.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
             alt=""
           />
@@ -48,13 +126,31 @@ export const Overview = () => {
         </div>
         <div className="flex flex-col">
           <img
-            className="object-cover mb-6 rounded-xl shadow-lg h-28 sm:h-48 xl:h-56 w-28 sm:w-48 xl:w-56 shadow-[#777] dark:shadow-cyan-500/50"
+            className="object-cover mb-6 rounded-xl shadow-lg h-56 w-56 shadow-[#777] dark:shadow-cyan-500/50"
             src="https://images.pexels.com/photos/3184287/pexels-photo-3184287.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
             alt=""
           />
           <p >BASICS OF HTML</p>
         </div>
-      </div>
+        <div className="flex flex-col">
+          <img
+            className="object-cover mb-6 rounded-xl shadow-lg h-56  w-56 shadow-[#777] dark:shadow-cyan-500/50"
+            src="https://images.pexels.com/photos/3184287/pexels-photo-3184287.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+            alt=""
+          />
+          <p >BASICS OF HTML</p>
+        </div>
+        <div className="flex flex-col">
+          <img
+            className="object-cover mb-6 rounded-xl shadow-lg h-56 w-56 shadow-[#777] dark:shadow-cyan-500/50"
+            src="https://images.pexels.com/photos/3184287/pexels-photo-3184287.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+            alt=""
+          />
+          <p >BASICS OF HTML</p>
+        </div>
+
+        </Slider>
+       
     </div>
   );
 };
